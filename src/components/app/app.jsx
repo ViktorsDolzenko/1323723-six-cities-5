75700f1df2fa642d4697e80e186389baa06b251c
AwSicker <vitja1234@gmail.com>
2020-10-03 04:07:44 +0300

    module2-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 import MainPage from "../main-page/main-page";
-
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import Favorites from "../favorites/favorites";
+import Login from "../login/login";
+import Property from "../property/property";
 
 const App = (props) => {
   const {offersCount} = props;
 
   return (
-    <MainPage offersCount={offersCount}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage offersCount={offersCount}/>
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/property">
+          <Property />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
   );
 };
 

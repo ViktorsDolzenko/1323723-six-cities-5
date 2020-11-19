2c7db67ba4a7c93189d725fe3e2fab4bdab0a836
AwSicker <vitja1234@gmail.com>
2020-11-19 23:21:34 +0200

    module7-task2

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,17 +4,27 @@ import {MainPage} from "../main-page/main-page";
 import {Favorites} from "../favorites/favorites";
 import {Login} from "../login/login";
 import {Property} from "../property/property";
+import {PrivateRoute} from "../private-route/private-route";
 
 export function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={MainPage}>
-        </Route>
-        <Route exact path="/login" component={Login}>
-        </Route>
-        <Route exact path="/favorites" component={Favorites}>
-        </Route>
+        <Route exact path="/" component={MainPage}/>
+        <PrivateRoute
+          exact
+          path="/login"
+          render={() =>(
+            <Login/>
+          )}
+        />
+        <PrivateRoute
+          exact
+          path="/favorites"
+          render={() =>(
+            <Favorites/>
+          )}
+        />
         <Route exact path="/offer/:id" component={Property}/>
       </Switch>
     </BrowserRouter>
--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,13 +1,16 @@
 import {combineReducers} from "redux";
 import {appProcess} from "./reducers/app-process/app-process";
 import {appData} from "./reducers/app-data/app-data";
+import {user} from "./reducers/user/user";
 
 export const NameSpace = {
   DATA: `DATA`,
   PROCESS: `PROCESS`,
+  USER: `USER`
 };
 
 export const reducers = combineReducers({
   [NameSpace.PROCESS]: appProcess,
   [NameSpace.DATA]: appData,
+  [NameSpace.USER]: user
 });
